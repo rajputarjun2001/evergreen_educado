540c541e12acbbced42db3b4538b58f6c3f6b3ec
Tanuja Tiwari <tanujatiwari04@gmail.com>
2021-03-25 20:51:59 +0000

    completed new backend paradigm

--- app.js
+++ app.js
@@ -79,6 +79,199 @@ app.get("/cat/:id",function(req,res){
 	});
 });
 
+
+// --------------------NEET--------------------------------
+
+app.get("/neet",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("neet/index",{book: book});	
+	});
+});
+
+app.get("/neet/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("neet/show",{book:book});
+	});
+});
+
+
+// --------------------JEE MAINS--------------------------------
+
+app.get("/jee-mains",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("jee/index",{book: book});	
+	});
+});
+
+app.get("/jee-mains/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("jee/show",{book:book});
+	});
+});
+
+
+// --------------------CLAT--------------------------------
+
+app.get("/clat",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("clat/index",{book: book});	
+	});
+});
+
+app.get("/clat/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("clat/show",{book:book});
+	});
+});
+
+
+// --------------------NDA--------------------------------
+
+app.get("/nda",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("nda/index",{book: book});	
+	});
+});
+
+app.get("/nda/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("nda/show",{book:book});
+	});
+});
+
+// --------------------GATE--------------------------------
+
+app.get("/gate-categories",function(req,res){
+	res.render("gate/index");	
+});
+
+//CSE
+
+app.get("/gate-cse",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("gate/cse/index",{book: book});	
+	});
+});
+
+app.get("/gate-cse/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("gate/cse/show",{book:book});
+	});
+});
+
+//ELECTRICAL
+
+app.get("/gate-electrical",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("gate/electrical/index",{book: book});	
+	});
+});
+
+app.get("/gate-electrical/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("gate/electrical/show",{book:book});
+	});
+});
+
+
+//CIVIL
+
+app.get("/gate-civil",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("gate/civil/index",{book: book});	
+	});
+});
+
+app.get("/gate-civil/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("gate/civil/show",{book:book});
+	});
+});
+
+
+//ELECTRONICS
+
+app.get("/gate-electronics",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("gate/electronics/index",{book: book});	
+	});
+});
+
+app.get("/gate-electronics/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("gate/electronics/show",{book:book});
+	});
+});
+
+//MECHANICAL
+
+app.get("/gate-mechanical",function(req,res){
+	Book.find({},function(err,book){
+		if(err)
+			console.log(err)
+		else
+		res.render("gate/mechanical/index",{book: book});	
+	});
+});
+
+app.get("/gate-mechanical/:id",function(req,res){
+	Book.findById(req.params.id).populate("comments").exec(function(err,book){
+		if(err)
+			console.log(err);
+		else
+			res.render("gate/mechanical/show",{book:book});
+	});
+});
+
+
 // ---------------------------COMMON ROUTES---------------------------------
 
 app.get("/admin",function(req,res){
@@ -125,15 +318,14 @@ app.get("/admin/books/:id/delete",middlewareObj.isAdmin,function(req,res){
 		if(err)
 			console.log("Didnt delete",err)
 		else
-			res.redirect("/cat")
+			res.redirect("/admin/books")
 	});
 });
 
 // ---------------------------COMMENTS-------------------------------
 
-
 // CAT
-app.get("/cat/:id/comments/new",function(req,res){
+app.get("/cat/:id/reviews/new",function(req,res){
 	Book.findById(req.params.id,function(err,book){
 		if(err)
 			console.log(err)
@@ -142,7 +334,7 @@ app.get("/cat/:id/comments/new",function(req,res){
 	});
 });
 
-app.post("/cat/:id/comments",function(req,res){
+app.post("/cat/:id/reviews",function(req,res){
 	Book.findById(req.params.id,function(err,book){
 		if(err){
 			console.log(err);
@@ -161,7 +353,7 @@ app.post("/cat/:id/comments",function(req,res){
 	});
 });
 
-app.delete("/cat/:id/comments/:comment_id",middlewareObj.isAdmin,function(req,res){
+app.delete("/cat/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
 	Book.findById(req.params.id,function(err,book){
 		if(err)
 			console.log(err)
@@ -176,6 +368,421 @@ app.delete("/cat/:id/comments/:comment_id",middlewareObj.isAdmin,function(req,re
 		}
 	});
 });
+
+// NEET
+app.get("/neet/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/neet/new",{book : book});
+	});
+});
+
+app.post("/neet/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/neet")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/neet/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/neet/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/neet/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
+// JEE MAINS
+app.get("/jee-mains/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/jee/new",{book : book});
+	});
+});
+
+app.post("/jee-mains/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/jee-mains")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/jee-mains/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/jee-mains/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/jee-mains/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
+// CLAT
+app.get("/clat/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/clat/new",{book : book});
+	});
+});
+
+app.post("/clat/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/clat")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/clat/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/clat/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/clat/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
+
+// NDA
+app.get("/nda/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/nda/new",{book : book});
+	});
+});
+
+app.post("/nda/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/nda")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/nda/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/nda/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/nda/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
+//GATE CSE
+
+app.get("/gate-cse/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/gate/cse/new",{book : book});
+	});
+});
+
+app.post("/gate-cse/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/gate-cse")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/gate-cse/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/gate-cse/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/gate-cse/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
+
+//GATE ELECTRICAL
+
+app.get("/gate-electrical/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/gate/electrical/new",{book : book});
+	});
+});
+
+app.post("/gate-electrical/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/gate-electrical")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/gate-electrical/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/gate-electrical/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/gate-electrical/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
+
+//GATE CIVIL
+
+app.get("/gate-civil/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/gate/civil/new",{book : book});
+	});
+});
+
+app.post("/gate-civil/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/gate-civil")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/gate-civil/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/gate-civil/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/gate-civil/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
+
+//GATE ELECTRICAL
+
+app.get("/gate-electronics/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/gate/electronics/new",{book : book});
+	});
+});
+
+app.post("/gate-electronics/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/gate-electronics")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/gate-electronics/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/gate-electronics/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/gate-electronics/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
+// GATE MECHANICAL
+
+app.get("/gate-mechanical/:id/reviews/new",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else
+			res.render("comments/gate/mechanical/new",{book : book});
+	});
+});
+
+app.post("/gate-mechanical/:id/reviews",function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err){
+			console.log(err);
+			res.redirect("/gate-mechanical")
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err)
+				else
+					book.comments.push(comment);
+					book.save();
+					res.redirect("/gate-mechanical/" + book._id);
+			});
+		}
+	});
+});
+
+app.delete("/gate-mechanical/:id/reviews/:comment_id",middlewareObj.isAdmin,function(req,res){
+	Book.findById(req.params.id,function(err,book){
+		if(err)
+			console.log(err)
+		else{
+			Comment.findByIdAndRemove(req.params.comment_id,function(err){
+				if(err)
+					console.log(err)
+				else{
+					res.redirect("/gate-mechanical/"+req.params.id);
+				}
+			});
+		}
+	});
+});
+
 
 // ---------------------ABOUT-----------------------------
 
